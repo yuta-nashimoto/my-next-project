fa72ff39c78f11543a8527dc39337bf2c13b8b0d
y.n <yuta.business01@gmail.com>
2024-09-30 17:42:14 +0900

    3章まで完了

---
+++ app/_components/NewsList/index.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import styles from "./index.module.css";
+import type { News } from "@/app/_libs/microcms";
+import Date from "../Date";
+import Category from "../Category";
+
+type Props = {
+  news: News[];
+};
+
+export default function NewsLis({ news }: Props) {
+  if (news.length === 0) {
+    return <p>記事がありません。</p>;
+  }
+  return (
+    <ul>
+      {news.map((article) => (
+        <li key={article.id} className={styles.list}>
+          <div className={styles.link}>
+            <Image
+              className={styles.image}
+              src="/no-image.png"
+              alt="no-image"
+              width={1200}
+              height={630}
+            />
+            <dl className={styles.content}>
+              <dt className={styles.title}>{article.title}</dt>
+              <dd className={styles.meta}>
+                <Category category={article.category} />
+                <Date date={article.publishedAt ?? article.createdAt} />
+              </dd>
+            </dl>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
