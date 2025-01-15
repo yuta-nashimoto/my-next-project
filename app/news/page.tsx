6be927f8458610c385d92aa2ccddb17326c6c988
y.n <yuta.business01@gmail.com>
2025-01-15 11:34:40 +0900

    ニュースにSSRを適用

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -4,6 +4,8 @@ import { NEWS_LIST_LIMIT } from "@/app/_constants";
 import Pagination from "../_components/Pagination";
 import SearchFiled from "../_components/SearchFileld";
 
+export const revalidate = 0;
+
 export default async function Page() {
   const { contents: news, totalCount } = await getNewsList({
     limit: NEWS_LIST_LIMIT,
